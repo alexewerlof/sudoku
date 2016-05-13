be1af413918e92bba8b77f92142740ebb1b4f486
Alex Ewerlöf <hanifbbz@gmail.com>
2016-05-13 20:16:54 +0200

    added some test sudoku table

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Board {
       let currentRow = [];
       this.cells.push(currentRow);
       for (let x = 0; x < 9; x++) {
-        currentRow[x] = '.';
+        currentRow[x] = ' ';
       }
     }
   }
@@ -17,7 +17,27 @@ class Board {
     }
     return ret.join('\n');
   }
+
+  setCells(...rows) {
+    for (let r = 0; r < 9; r++) {
+      for (let x = 0; x < 9; x++) {
+        this.cells[r][x] = rows[r].charAt(x);
+      }
+    }
+  }
 }
 
 var b = new Board;
+console.log(b.toString());
+b.setCells(
+  '    27  8',
+  ' 3   826 ',
+  ' 8  463 7',
+  '6  28    ',
+  '5  7  48 ',
+  '2  7  48 ',
+  '  5 7  4 ',
+  '8 1  372 ',
+  '  78546  '
+)
 console.log(b.toString());
